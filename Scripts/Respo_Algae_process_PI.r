f9fe4c4f428cef6b05925254e2687a2f8b65bba0
haleypoppinga <hpopping@hawaii.edu>
2026-02-10 07:19:36 -1000

    PI update

--- Scripts/Respo_Algae_process_PI.r
+++ Scripts/Respo_Algae_process_PI.r
@@ -1,4 +1,4 @@
-###### Respo Code for Algae Light and Dark Runs ####### 
+###### Respo Code for Algae PI Curve ####### 
 ### Created by: Haley Poppinga, Maya Powell, Nyssa Silbiger
 #### Last updated on: 2026-02-09
 
@@ -40,6 +40,7 @@ library(car)
 library(future)
 library(furrr)
 library(dplyr)
+library(beepr)
 
 ############# now it's time to code ############
 ################################################
@@ -47,7 +48,7 @@ library(dplyr)
 
 #set the path to all of the raw oxygen datasheets
 ## these are saved onto the computer in whatever file path/naming scheme you saved things to 
-path.p<-here("Data","Respo_Files","PI") #the location of all your respirometry files
+path.p<-here("Data","Respo_Files","PI","RawO2") #the location of all your respirometry files
 #you can change to individual run folders if needed
 
 # bring in all of the individual files
@@ -74,12 +75,9 @@ RespoMeta <- read_csv(here("Data","Respo_Files","PI","Algae_PI_meta.csv"))
 
 # join the data together
 Sample_Info <- left_join(RespoMeta, BioData)
-#View(Sample.Info)
+#View(Sample_Info)
 
 
-# join the data together
-Sample_Info <- left_join(RespoMeta, BioData)
-#View(Sample.Info)
 
 ##### Make sure times are consistent ####
 # make start and stop times real times, so that we can join the respo output and sample_info data frames
@@ -87,9 +85,10 @@ Sample_Info <- Sample_Info %>%
   #drop_na(sample_ID) %>% 
   unite(date,start_time,col="start_time",remove=F, sep=" ") %>% 
   unite(date,stop_time,col="stop_time",remove=F, sep=" ") %>%
-  mutate(start_time = mdy_hms(start_time)) %>% 
-  mutate(stop_time = mdy_hms(stop_time)) %>% 
-  mutate(date = mdy(date))
+  mutate(start_time = ymd_hms(start_time)) %>% 
+  mutate(stop_time = ymd_hms(stop_time)) %>% 
+  mutate(date = ymd(date)) %>% 
+  mutate(light_level = as.integer(light_level))
 
 #view(Sample_Info)
 
@@ -98,27 +97,38 @@ Sample_Info <- Sample_Info %>%
 
 n_light_levels<-8 # number of unique light levels
 
-RespoR <- tibble(.rows =length(filenames_final)*n_light_levels,
-                 sample_id = NA,
-                 intercept = NA,
-                 umol.L.sec = NA,
-                 temp_c = NA,
-                 light_level = NA,
-                 light_value = NA,
-                 run_block = NA)
+RespoR <- tibble(.rows = length(filenames_final)*n_light_levels,
+                 file_id_csv = NA_character_,
+                 sample_id = NA_character_,
+                 intercept = NA_real_,
+                 umol.L.sec = NA_real_,
+                 temp_c = NA_real_,
+                 light_level = NA_integer_,
+                 light_value = NA_real_,
+                 run_block = NA_character_)
+
+
+
+# create directory for output folder
+out_dir <- here("Output", "PI")
+dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
+
+
 
 ######### Create a for loop! ###############
 ############################################
 
 ###forloop#####
-for(i in 1:length(filenames_final)) {
+for(i in 1:length(filenames_final)) { # loop each raw file?
   FRow <- as.numeric(which(Sample_Info$file_id_csv==filenames_final[i])) # stringsplit this renames our file
   
   Respo.Data1 <- read_csv(skip=1,file.path(path.p, paste0(file.names.full[i]))) %>% # reads in each file in list
     dplyr::select(Date, Time, Value, Temp) %>% # keep only what we need: Time stamp per 1sec, Raw O2 value per 1sec, in situ temp per 1sec
     unite(Date,Time,col="Time",remove=T, sep = " ") %>%
     drop_na() %>% 
-    mutate(Time = mdy_hms(Time)) #%>% # convert time
+    mutate(Time = mdy_hms(Time), Value = as.numeric(Value), Temp  = as.numeric(Temp))
+    #filter(Time >= start_time & Time <= stop_time)
+    #mutate(Time = ymd_hms(Time)) #%>% # convert time
   #mutate(help = i) ##if stuck in forloop with error from filter, can check RespoR and see at what row the forloop stopped working  
   
   ## cut the data by start and stop times from metadata
@@ -183,11 +193,11 @@ for(i in 1:length(filenames_final)) {
     nest_by(light_level) %>%
     ungroup()%>%
     mutate(regs = furrr::future_map(data, fit_reg), # run the LOLinR fit in parallel
-           Temp.C = map_dbl(map(data, "Temp"), mean),# get the mean temperature
+           temp_c = map_dbl(map(data, "Temp"), mean),# get the mean temperature
            RegStats =map(regs, function(x){ # extract the intercept and slope for the parameters
              x$allRegs %>%
                slice(1) %>%
-               select(Intercept = b0,
+               select(intercept = b0,
                       umol.L.sec = b1)
            }) )
   
@@ -195,46 +205,81 @@ for(i in 1:length(filenames_final)) {
   #  Plot regression diagnostics
   
   for(j in 1:length(df$light_level)){
-    pdf(paste0(here("Output","PI"),"/",rename,"_",j,".pdf" ))
+    pdf(file.path(out_dir, paste0(rename, "_", j, ".pdf")))
     plot(df$regs[[j]])
     dev.off() 
   }
   
   
-  df<-df %>%
-    select(light_level, temp_c, RegStats ) %>%
+  # attach meta for this file (by light_level) - minimal and reliable
+  meta_steps <- Sample_Info[FRow,] %>% select(light_level, light_value, run_block)
+  
+  df <- df %>%
+    select(light_level, temp_c, RegStats) %>%
     unnest(RegStats) %>%
-    mutate(sample_id = rename) %>%
-    left_join(Sample_Info[FRow,] %>%
-                select(sample_id, light_level, light_value, run_block)) # make sure the light value (or whatever other metadata you want) is in the final DF
+    left_join(meta_steps, by="light_level") %>%
+    mutate(sample_id = rename)   # keep your original sample_id style
+
+
+  
   
   ################################
-  # fill in all the O2 consumption and rate data
+  # index for the 8 light steps of this file
+  idx <- ((i - 1) * n_light_levels + 1):(i * n_light_levels)
   
-  RespoR[FRow,"temp_c"]<-df$Temp.C
-  RespoR[FRow,"sample_id"]<-df$sample_ID
-  RespoR[FRow,"intercept"]<-df$Intercept
-  RespoR[FRow,"umol.L.sec"]<-df$umol.L.sec
-  RespoR[FRow,"light_level"]<-df$light_level
-  RespoR[FRow,"light_value"]<-df$light_value
-  RespoR[FRow,"run_block"]<-df$run_block
+  # store file id for join later
+  RespoR[idx, "file_id_csv"] <- filenames_final[i]
+  
+  
+  # fill in all the O2 consumption and rate data
+  RespoR[idx,"temp_c"]      <- df$temp_c
+  RespoR[idx,"sample_id"]   <- df$sample_id
+  RespoR[idx,"intercept"]   <- df$intercept
+  RespoR[idx,"umol.L.sec"]  <- df$umol.L.sec
+  RespoR[idx,"light_level"] <- df$light_level
+  RespoR[idx,"light_value"] <- df$light_value
+  RespoR[idx,"run_block"]   <- df$run_block
   
 }  
-
 
 ######### end of for loop - celebrate victory of getting through that ###############
 ############################################
 
+
+
+
 #export raw data and read back in as a failsafe 
 #this allows me to not have to run the for loop again !!!!!
-write_csv(RespoR, here("Data","RespoFiles","PI","Respo_Algae_R.csv"))  
+write_csv(RespoR, here("Data","Respo_Files","PI","Respo_Algae_R.csv"))  
 
 ##### 
 
-RespoR <- read_csv(here("Data","RespoFiles","PI","Respo_Algae_R.csv"))
+RespoR <- read_csv(here("Data","Respo_Files","PI","Respo_Algae_R.csv"), show_col_types = FALSE) %>%
+  select(file_id_csv, light_level, intercept, umol.L.sec, temp_c) %>%
+  mutate(light_level = as.integer(light_level))
 
 ######### Calculate Respiration rate ###############
 ############################################
+
+#avoid duplicated columns after join (run_block/light_value already exist in RespoR)
+RespoR2 <- RespoR %>%
+  left_join(Sample_Info %>% mutate(light_level = as.integer(light_level)),
+            by = c("file_id_csv", "light_level")) %>%
+  mutate(Ch.Volume.mL = volume_mL,
+         Ch.Volume.L  = Ch.Volume.mL * 0.001,
+         umol.sec = umol.L.sec * Ch.Volume.L)
+
+#Account for blank rate by sample run Block (if we do at least one blank per block)
+
+#View(RespoR)
+
+####### normalize the respo rates to the blanks ##### 
+######### Blank correction (explicit join; keeps run_block intact) #########
+# 1) calculate blank rate per (light_level x run_block x light_dark)
+#blank_rates <- RespoR2 %>%
+#  filter(blank == 1) %>%
+#  group_by(light_level, run_block, light_dark) %>%
+#  summarise(blank.rate = mean(umol.sec, na.rm = TRUE), .groups = "drop")
 
 RespoR2 <- RespoR %>%
   #drop_na(FileID_csv) %>% # drop NAs
@@ -256,42 +301,51 @@ RespoR_Normalized <- RespoR2 %>%
   #dplyr::select(blank.rate = umol.sec) %>% ## rename the blank column 
   summarise(blank.rate = mean(umol.sec, na.rm = TRUE)) %>% # if you have multiple blanks per run take the average
   ungroup() %>% 
+# join blank rates back onto ALL data, then correct, then normalize by wet weight
+#RespoR_Normalized <- RespoR2 %>%
+#  left_join(blank_rates, by = c("run_block", "light_dark")) %>%
   #dplyr::select(Light_level, run_block, blank.rate, date) %>% # this is what I will use to join the blanks back with the raw data
   right_join(RespoR2) %>% # join blanks with the respo data
-  mutate(umol.sec.corr = umol.sec - blank.rate, # subtract the blank rates from the raw rates   
-         umol.cm2.hr = (umol.sec.corr*3600)/wetweight_g,
-         umol.cm2.hr_uncorr = (umol.sec*3600)/wetweight_g) %>%
-  # mmol.cm2.hr = 0.001*(umol.sec.corr*3600)/SA_cm2, # convert to mmol cm-2 hr-1
-  # mmol.cm2.hr_uncorr = 0.001*(umol.sec*3600)/SA_cm2) %>% 
-  filter(blank!=1) %>% # remove the Blank data
-  #ungroup() %>%
-  dplyr::select(date, species, sample_id, algae_id, light_dark, run_block, wetweight_g, run_block, umol.cm2.hr, chamber_channel, 
-                temp_c, umol.cm2.hr_uncorr, light_level, light_value, light_dark) #keep only what we need
+  mutate(umol.sec.corr   = umol.sec - blank.rate, # subtract the blank rates from the raw rates
+         umol.g.hr = (umol.sec.corr * 3600) / wetweight_g,
+         umol.g.hr_uncorr= (umol.sec * 3600) / wetweight_g) %>%
+  filter(blank != 1) %>%  # remove blank rows from the "sample" dataset, # remove the Blank data
+  dplyr::select(date, species, sample_id, algae_id, light_dark, run_block, wetweight_g, chamber_channel,
+                temp_c, light_level, light_value,umol.sec, blank.rate, umol.sec.corr, 
+                umol.g.hr, umol.g.hr_uncorr) #keep only what we need
 
 
 #######################
 ### making a df for just blank data for future use in plots ### 
+#Blank_only <- blank_rates %>% 
+#  left_join(Sample_Info %>% distinct(light_level, run_block, light_dark, light_value),
+#            by = c("Light_level" = "light_level","run_block","light_dark"))
+### making a df for just blank data for future use in plots ### 
+
 Blank_only <- RespoR2 %>% 
   filter(blank == 1) %>% # grab the blanks
   group_by(light_level, light_value, run_block,light_dark) %>%
   #dplyr::select(blank.rate = umol.sec) %>% ## rename the blank column 
   summarise(blank.rate = mean(umol.sec, na.rm = TRUE))
 
-#############################
-write_csv(RespoR_Normalized , here("Data","RespoFiles","PI","Respo_Algae_RNormalized_AllPIRates.csv"))  
+write_csv(RespoR_Normalized , here("Data","Respo_Files","PI","Respo_Algae_RNormalized_AllPIRates.csv"))  
 
 
 ## Plot the blanks across treatments to make sure nothing is funky
 Blank_only %>%
-  ggplot(aes(x = light_value, blank.rate)) +
-  geom_point()
+  ggplot(aes(x = light_value, blank.rate, group = interaction(run_block, light_dark))) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~run_block)
 
 #  basic plot of rates versus light before you make the real PI curve 
 basic_PI_plot <- RespoR_Normalized %>%
-  ggplot(aes(x = light_value, y = umol.cm2.hr, color = species, group = algae_id))+
+  ggplot(aes(x = light_value, y = umol.g.hr, color = species, group = algae_id)) +
   geom_point()+
   geom_line()+
-  facet_wrap(~species, scales = "free")
+  facet_wrap(~species, scales = "free_y")
 ggsave(here("Output","PI","basic_PI_plot.pdf"), basic_PI_plot)
 
+
 ### run an nls model for PI curve and extract Ik for each species ###
+beep(sound = 8, expr = NULL)
